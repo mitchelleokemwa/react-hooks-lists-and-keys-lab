b24c7a473fcc9d1ca0124ffbf4373401e023141a
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-02-08 12:42:07 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 function NavBar() {
-	const links = ["home", "about", "projects"];
+  const links = ["home", "about", "projects"];
 
-	return (
-		<nav>
-			{links.map((link) => (
-				<a href={"#" + link} key={link}>
-					{link}
-				</a>
-			))}
-		</nav>
-	);
+  return <nav>{/* display an <a> tag for each link here */
+  links.map(link => <a key={link} href={`#${link}`}>{link}</a>)
+  }</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-	return (
-		<div className="project-item">
-			<h3>{name}</h3>
-			<p>{about}</p>
-			<div className="technologies">
-				{technologies.map((tech) => (
-					<span key={tech}>{tech}</span>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="project-item">
+      <h3>{name}</h3>
+      <p>{about}</p>
+      <div className="technologies">{/* render a <span> for each technology in the technologies array */
+        technologies.map(technology => <span key={technology}>{technology}</span>)
+        }
+      </div>
+    </div>
+  );
 }
 
 export default ProjectItem;
